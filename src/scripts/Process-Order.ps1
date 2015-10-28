37bb99547d5d8767e0eefefc964838b64317a8af
rufer7 <marc.rufer@d-fens.net>
2015-10-28 10:10:46 +0100

    Initial commit
    * Process-Order script
    * Process-VDIAssignment script

---
+++ src/scripts/Process-Order.ps1
@@ -0,0 +1,93 @@
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+. "$here\Process-VDIAssignment.ps1"
+
+
+function ProcessOrder($svc, $orderJob) {
+	
+	$tenantNodeName = 'TenantNode';
+	
+	# Check if tenantNode exists
+	$filterQuery = "startswith(Name, '{0}') eq true" -f $tenantNodeName;
+	$tenantNode = $svc.Core.Nodes.AddQueryOption('$filter', $filterQuery) | Select;
+	
+	if($null -eq $tenantNode)
+	{
+		$msg = "TenantNode does not exist for tenant {0}" -f $orderJob.Tid;
+		Write-Host $msg;
+		return;
+	}
+	
+	# Load order from job
+	$order = $svc.Core.Orders.AddQueryOption('$filter', "Id eq " + $orderJob.ReferencedItemId) | Select;
+
+	# Load order VDI order items
+	# DFTODO - Adjust query to load orderItems of type VDI
+	$orderItems = $svc.Core.LoadProperty($order, 'OrderItems') | Select;
+	
+	# DFTODO - First handle VDI item if exists ($product.Type -eq 'VDI')
+	if()
+	{
+		# DFTODO - Check for existing VDI item -> error if already existing (handle requester of Order)
+		
+
+		# Load product of orderItem
+		$product = $svc.Core.LoadProperty($orderItem, 'Product') | Select;
+		
+		$result = ProcessVDIAssignment;
+		
+		# Create VDI inventory entry
+		if($result -eq $true)
+		{			
+			CreateInventoryEntry -svc $svc -parentNode $tenantNode -product $product;
+		}
+		else 
+		{
+			UpdateOrder -svc $svc -order $order -status 'Cancel' -errorMsg $result;
+			return;
+		}
+	}
+	
+	# DFTODO - Load non VDI orderItems
+	foreach($orderItem in $orderItems)
+	{
+		# DFTODO - Implement handling of SW Package OrderItems
+		# DFTODO - Set VDI node item as parent
+		# DFTODO - Handle requester of Order
+	}
+}
+
+function CreateInventoryEntry($svc, $parentNode, $product) 
+{
+	$node = New-Object biz.dfch.CS.Appclusive.Api.Core.Node;
+	$svc.Core.AddToNodes($node);
+	$node.Name = $product.Name;
+	$node.Description = $product.Description;
+	$node.Parameters = '{}';
+	$node.Type = $product.Type;
+	$node.Created = [System.DateTimeOffset]::Now;
+	$node.Modified = $node.Created;
+	$node.CreatedBy = "SYSTEM";
+	$node.ModifiedBy = $node.CreatedBy;
+	$node.Tid = "1";
+	$node.Id = 0;
+	$node.ParentId = $parentNode.Id;
+	$svc.Core.UpdateObject($node);
+	$svc.Core.SaveChanges();
+}
+
+function UpdateOrder($svc, $order, $status, $errorMsg)
+{
+	# Update order
+	try
+	{
+		$order.Status = $status;
+		$order.Parameters = $errorMsg;
+		$svc.Core.UpdateObject($order);
+	}
+	catch
+	{
+		# DFTODO - Handle exception correctly -> an exception is thrown in any case because of PATCH/PUT Order returns Job!
+		# Write-Host ("Changing status of order '{0}' [{1}] FAILED.{2}{3}" -f $order.Name, $order.Id, [Environment]::NewLine, ($order | Out-String));
+	}
+}
---
+++ src/scripts/Process-VDIAssignment.ps1
@@ -0,0 +1,10 @@
+function ProcessVDIAssignment() 
+{
+	$success = $true;
+	
+	# DFTODO - Handle VDI order item
+	# DFTODO - Check that a VDI doesn't exist already -> set error (Parameters??)
+	# DFTODO - Assign VDI
+	
+	return $success;
+}
