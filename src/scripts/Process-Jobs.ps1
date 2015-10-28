01d4387c407d8021be2b6a45d9d67608c1c6df53
rufer7 <marc.rufer@d-fens.net>
2015-10-28 09:04:05 +0100

    first version of process-jobs added

---
+++ src/scripts/Process-Jobs.ps1
@@ -0,0 +1,30 @@
+#requires -Modules 'biz.dfch.PS.Appclusive.Client'
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+. "$here\Process-Order.ps1"
+
+$svc = Enter-AppclusiveServer;
+
+# Get jobs with status 'WaitingToRun'
+$waitingJobs = $svc.Core.Jobs.AddQueryOption('$filter', "Status eq 'WaitingToRun'") | Select;
+
+foreach($job in $waitingJobs) {
+
+	# Get job type
+	$separatorPos = $job.Name.LastIndexOf(".");
+	$jobType = $job.Name.Substring($separatorPos + 1);
+	
+	# Delegation to worker script based on job type
+	switch ($jobType) 
+    { 
+        "Order" 
+		{
+			ProcessOrder -svc $svc -job $job;
+		} 
+        default 
+		{
+			Write-Host ("JobType of job '{0}' [{1}] could not be determined.{2}{3}" -f $job.Name, $job.Id, [Environment]::NewLine, $job);
+		}
+    }
+}
