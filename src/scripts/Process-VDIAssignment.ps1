f8a0fc6333220a1eb7b7276849352533fc561850
rufer7 <marc.rufer@d-fens.net>
2015-10-28 14:01:26 +0100

    Further implementation of VDIAssignment script

--- src/scripts/Process-VDIAssignment.ps1
+++ src/scripts/Process-VDIAssignment.ps1
@@ -1,39 +1,57 @@
 function ProcessVDIEntitlement($username) 
 {
-	$result = $true;
-		
-	$result = CheckForExistingVDI -username $username;
-	if($true -eq $result)
+	$success = $true;
+	$username = $username.split('\')[1];
+
+	# DFTODO - Decide stubbing based on KNV entry
+	# DFTODO - Get the following properties from KNV? (add to seed.ps1?) + error handling
+	$connectionServerName = '';
+	$psSessionConfig = '';
+	$poolId = '';
+	
+	# DFTODO - Implement fallback to other connection server
+	Enter-PSSession -ComputerName $connectionServerName -ConfigurationName $psSessionConfig
+	
+	Add-PSSnapin VMware.View.Broker;
+	
+	$user = $null;
+	try 
 	{
-		$errorMsg = "User {0} has already a VDI assigned" -f $username;
+		$user = Get-User -name $username -ErrorAction Stop;
+	}
+	catch
+	{
+		$errorMsg = "User '{0}' not found" -f $username;
 		return $errorMsg;
 	}
 	
-	# DFTODO - Decide stubbing based on KNV entry
-	# DFTODO - Get the following properties from KNV? (add to seed.ps1?) + error handling
-	$computerName = '';
-	$psSessionConfig = '';
-	$poolId = ''
+	# Check if user already has a VDI
+	$vdi = GetExistingVDI -user $user;
+	if($vdi)
+	{
+		$errorMsg = "User '{0}' has already a VDI assigned to." -f $username;
+		return $errorMsg;
+	}
 	
-	# DFTODO - Implement fallback to other connection server
-	Enter-PSSession -ComputerName $computerName -ConfigurationName $psSessionConfig
+	# DFTODO - Implement fallback to other pools
+	try
+	{
+		$pool = Get-Pool -pool_id $poolId -ErrorAction Stop;
+	}
+	catch
+	{
+		$errorMsg = "Pool with Id '{0}' not found" -f $poolId;
+		return $errorMsg;
+	}
 	
-	$pool = Get-Pool -pool_id $poolId;
-	
-	
-	
-	Add-PSSnapin VMware.View.Broker
 	# DFTODO - Entitle VDI
 	
-	return $result;
+	
+	
+	return $success;
 }
 
-function CheckForExistingVDI($username) 
+function GetExistingVDI($user) 
 {
-	$result = $true;
-	
-	# DFTODO - Check if a VDI already exists for the given user
-	# DFTODO - Get connection server from MgmtUri or KNV!
-	
-	return $result
+	return Get-DesktopVM |? user_sid -eq $user.sid;
 }
