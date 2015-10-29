7be563aa510597bc890eb322cebea97b120174a7
rufer7 <marc.rufer@d-fens.net>
2015-10-29 17:11:32 +0100

    * Additional test for testing order workflow added to Order.Tests * VDIAssignment script finished

--- src/scripts/Process-VDIAssignment.ps1
+++ src/scripts/Process-VDIAssignment.ps1
@@ -1,34 +1,32 @@
 function ProcessVDIEntitlement($username) 
 {
 	$username = $username.split('\')[1];
+	$key = 'biz.dfch.PS.Sunrise.Daas.Scripts.VDI';
+	
+	$stubMode = Get-AppclusiveKeyNameValue -Key 'biz.dfch.PS.Sunrise.Daas.Scripts.VDI' -Name 'StubMode';
+	
+	if($stubMode.Value -eq 'True')
+	{
+		return $true;
+	}
 
-	# DFTODO - Decide stubbing based on KNV entry
-	# DFTODO - Get the following properties from KNV? (add to seed.ps1?) + error handling
-	# these entries will be read from a ManagementUri (and associated ManagementCredential if necessary)
-	# pool names may be read from KNV
-	$connectionServerName = '';
-	$psSessionConfig = '';
-	$poolId = '';
+	$connectionServerName = Get-AppclusiveKeyNameValue -Key 'biz.dfch.PS.Sunrise.Daas.Scripts.VDI' -Name 'ConnectionServerName';
+	$psSessionConfig = Get-AppclusiveKeyNameValue -Key 'biz.dfch.PS.Sunrise.Daas.Scripts.VDI' -Name 'PsSessionConfig';
+	$poolId = Get-AppclusiveKeyNameValue -Key 'biz.dfch.PS.Sunrise.Daas.Scripts.VDI' -Name 'PoolId';
 	
 	# DFTODO - Implement fallback to other connection server (also defined in ManagementUri)
-	Enter-PSSession -ComputerName $connectionServerName -ConfigurationName $psSessionConfig
+	Enter-PSSession -ComputerName $connectionServerName.Value -ConfigurationName $psSessionConfig.Value
 	
 	Add-PSSnapin VMware.View.Broker -ErrorAction:Stop;
 	
-	$user = $null;
-	try 
-	{
-		# DFTODO - here I suggest to use ErrorAction:SilentlyContinue and then just check for $null
-		# smae holds true for all other try/catch statements with ErrorAction:Stop
-		$user = Get-User -name $username -ErrorAction Stop;
-	}
-	catch
+	$user = Get-User -name $username -ErrorAction:SilentlyContinue;
+	if ($user -eq $null)
 	{
 		$errorMsg = "User '{0}' not found" -f $username;
 		return $errorMsg;
 	}
 	
-	# Check if user already has a VDI
+	# Check, if user already has a VDI
 	$entitlement = GetExistingEntitlement -user $user;
 	if($entitlement)
 	{
@@ -36,21 +34,18 @@ function ProcessVDIEntitlement($username)
 		return $errorMsg;
 	}
 	
-	try
+	$pool = Get-Pool -pool_id $poolId.Value -ErrorAction:SilentlyContinue;
+	if($pool -eq $null)
 	{
-		$pool = Get-Pool -pool_id $poolId -ErrorAction Stop;
-	}
-	catch
-	{
-		$errorMsg = "Pool with Id '{0}' not found" -f $poolId;
+		$errorMsg = "Pool with id '{0}' not found" -f $poolId.Value;
 		return $errorMsg;
 	}
 	
-	$result = $user | Add-PoolEntitlement -pool_id $poolId;
+	$result = $user | Add-PoolEntitlement -pool_id $poolId.Value;
 	
-	if ($result.EntitlementsAdded -ne 1) 
+	if($result.EntitlementsAdded -ne 1) 
 	{
-		$errorMsg = "Entitlement FAILED (PoolId: {0}, Username: {1})" -f $poolId, $username;
+		$errorMsg = "Entitlement FAILED (PoolId: {0}, Username: {1})" -f $poolId.Value, $username;
 		return $errorMsg;
 	}
 	
