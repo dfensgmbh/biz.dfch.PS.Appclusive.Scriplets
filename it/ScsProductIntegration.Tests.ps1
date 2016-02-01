82f889c02e527481c00298e5d1939837191266fa
rufer7 <marc.rufer@d-fens.net>
2016-02-01 14:03:06 +0100

    * test commented out to not be executed in automated tests

--- it/ScsProductIntegration.Tests.ps1
+++ it/ScsProductIntegration.Tests.ps1
@@ -22,32 +22,32 @@ Describe -Tags "ScsProductIntegration.Tests" "ProductIntegration.Tests" {
 			$svc = Enter-ApcServer;
 		}
 		
-		It "AddCartItemAndPlaceOrder-Succeeds" -Test {
+		# It "AddCartItemAndPlaceOrder-Succeeds" -Test {
 			
-			$svc = Enter-Apc;
-			$cartItem = New-Object biz.dfch.CS.Appclusive.Api.Core.CartItem;
-			$cartItem.Quantity = 1;
-			$cartItem.CatalogueItemId = 11;
-			$cartItem.Name = 'Managed Red Hat Enterprise Linux 7';
-			$cartItem.Description = 'Managed Red Hat Enterprise Linux 7';
+			# $svc = Enter-Apc;
+			# $cartItem = New-Object biz.dfch.CS.Appclusive.Api.Core.CartItem;
+			# $cartItem.Quantity = 1;
+			# $cartItem.CatalogueItemId = 11;
+			# $cartItem.Name = 'Managed Red Hat Enterprise Linux 7';
+			# $cartItem.Description = 'Managed Red Hat Enterprise Linux 7';
 			
-			$cartItem.Parameters = '{"vm":{"id":"3294","name":"TestNiklaus8","CimiId":"http://cminv-lab3ch-1.mgmt.sccloudpoc.net:8080/inventory/cimi/2/machines/7e31ef0d-6fe7-45d2-a06a-0d66022a1a2d"},"username":"admin","password":"admin","managementLevel":"full","supportTime":"officeHours","maintenanceWindow":"weekend3OfMonth","autoApplyPatches":true,"emergencyPatches":true,"frozenZone":[{"dateTimeFrom":"2016-02-16T23:00:00.000Z","dateTimeTo":"2016-02-23T23:00:00.000Z"}],"customerAlarmingTargetList":"managedservices.cloud@swisscom.com"}';
+			# $cartItem.Parameters = '{"vm":{"id":"3294","name":"TestNiklaus8","CimiId":"http://cminv-lab3ch-1.mgmt.sccloudpoc.net:8080/inventory/cimi/2/machines/7e31ef0d-6fe7-45d2-a06a-0d66022a1a2d"},"username":"admin","password":"admin","managementLevel":"full","supportTime":"officeHours","maintenanceWindow":"weekend3OfMonth","autoApplyPatches":true,"emergencyPatches":true,"frozenZone":[{"dateTimeFrom":"2016-02-16T23:00:00.000Z","dateTimeTo":"2016-02-23T23:00:00.000Z"}],"customerAlarmingTargetList":"managedservices.cloud@swisscom.com"}';
 			
-			$svc.Core.AddToCartItems($cartItem);
-			$svc.Core.SaveChanges();
-			$svc.core.Carts
+			# $svc.Core.AddToCartItems($cartItem);
+			# $svc.Core.SaveChanges();
+			# $svc.core.Carts
 
-			$order = New-Object biz.dfch.CS.Appclusive.Api.Core.Order;
-			$order.Name = "Managed Red Hat Enterprise Linux 7";
-			$order.Description = "Managed Red Hat Enterprise Linux 7";
-			$order.Parameters = @{NodeId = "1680"} | ConvertTo-Json -Compress;
-			$svc.Core.AddToOrders($order);
-			$svc.Core.SaveChanges();
+			# $order = New-Object biz.dfch.CS.Appclusive.Api.Core.Order;
+			# $order.Name = "Managed Red Hat Enterprise Linux 7";
+			# $order.Description = "Managed Red Hat Enterprise Linux 7";
+			# $order.Parameters = @{NodeId = "1680"} | ConvertTo-Json -Compress;
+			# $svc.Core.AddToOrders($order);
+			# $svc.Core.SaveChanges();
 
-			# $approvals = $svc.Core.Approvals | Select
-			# $approval = $approvals[$approvals.Count - 1]
-			# $params = @{Name = "Continue"; Parameters="my reason"}
-			# Invoke-ApcEntityAction -InputObject $approval -EntityActionName "InvokeAction" -InputParameters $params;
-		}
+			# # $approvals = $svc.Core.Approvals | Select
+			# # $approval = $approvals[$approvals.Count - 1]
+			# # $params = @{Name = "Continue"; Parameters="my reason"}
+			# # Invoke-ApcEntityAction -InputObject $approval -EntityActionName "InvokeAction" -InputParameters $params;
+		# }
 	}
 }
