0b26531533fad48afe71e7a76ba6ade32ee21cd5
Ronald Rink <ronald.rink@d-fens.net>
2016-07-14 10:34:14 +0200

    Enter-Appclusive
    * added function for $profile that you can use to easily login to different environments

---
+++ src/Enter-Appclusive.ps1
@@ -0,0 +1,65 @@
+function Enter-Appclusive {
+PARAM
+(
+	[ValidateSet('LAB3', 'INT11')]
+	$Environment = 'LAB3'
+	,
+	$Username = 'Administrator'
+)
+
+	$here = (Get-Item $profile).Directory;
+	Push-Location $here;
+
+	try
+	{
+		$credentialFileName = 'Cred-Appclusive-{0}-{1}.xml' -f $Username, $Environment;
+		$credentialPathAndFileName = Join-Path -Path $PWD -ChildPath $credentialFileName;
+		
+		$fileExists = Test-Path $credentialPathAndFileName;
+		if(!$fileExists)
+		{
+			Write-Error ("Credential file '{0}' does not exist. Cannot login." -f $credentialPathAndFileName);
+			return;
+		}
+
+		$cred = Import-CliXml $credentialPathAndFileName;
+
+		Remove-Module biz.dfch.PS.Appclusive.Client -Force -ErrorAction:SilentlyContinue;
+		Import-Module biz.dfch.PS.Appclusive.Client;
+
+		switch($Environment)
+		{
+			"LAB3"
+			{
+				$serverBaseUri = 'http://lab3.appclusive.example.com/Appclusive/';
+			}
+			"INT11"
+			{
+				$serverBaseUri = 'http://int11.appclusive.example.com/Appclusive/';
+			}
+			default
+			{
+				Write-Error ("Unknown environment '{0}'" -f $Environment);
+				return;
+			}
+		}
+
+		$biz_dfch_PS_Appclusive_Client.ServerBaseUri = $serverBaseUri;
+		$svc = Enter-ApcServer -Credential $cred;
+		Test-ApcStatus -Authenticate;
+
+		$versions = Get-ApcVersion -All;
+		$title = '{0} - {1} - Server {2} - Client {3}' -f $Environment, $serverBaseUri, $versions.BaseUri.ToString(), $versions.'biz.dfch.PS.Appclusive.Client'.ToString(); 
+		$host.UI.RawUI.WindowTitle = $title;
+
+		return $svc;
+	}
+	catch
+	{
+		Write-Error ("Credential file '{0}' does not exist. Cannot login." -f $credentialPathAndFileName);
+	}
+	finally
+	{
+		Pop-Location;
+	}
+}
