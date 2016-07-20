26a6e1543bd4a1374c27d8f9c604078a43752737
Ronald Rink <ronald.rink@d-fens.net>
2016-07-20 17:23:17 +0200

    Add-PermissionToRole
    * added comments
    * renamed file

--- src/Add-PermissionToRole.ps1
+++ src/Add-PermissionToRole.ps1
@@ -10,10 +10,12 @@ Contract-Assert (!!$svc)
 Contract-Assert (!!$RoleName)
 Contract-Assert (!!$PermissionName)
 
+# searches for the *START* of the given permission name
 $q = "startswith(Name, '{0}')" -f $PermissionName;
 $permissions = $svc.Core.Permissions.AddQueryOption('$filter', $q) | Select;
 Contract-Assert (!!permissions)
 
+# searches for the role name, unless you are UberAdmin you will only retrieve the role inside your tenant
 $q = "Name eq '{0}'" -f $RoleName;
 $roles = $svc.Core.Roles.AddQueryOption('$filter', $q).AddQueryOption('$expand', "Permissions") | Select;
 Contract-Assert (!!roles)
