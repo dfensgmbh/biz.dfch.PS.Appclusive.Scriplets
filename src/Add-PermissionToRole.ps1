6a2a9261c0431e4888252762f2c55b9a24144802
Ronald Rink <ronald.rink@d-fens.net>
2016-07-21 15:05:33 +0200

    Add-PermissionToRole
    * fixed missing variable prefix ($)

--- src/Add-PermissionToRole.ps1
+++ src/Add-PermissionToRole.ps1
@@ -13,12 +13,12 @@ Contract-Assert (!!$PermissionName)
 # searches for the *START* of the given permission name
 $q = "startswith(Name, '{0}')" -f $PermissionName;
 $permissions = $svc.Core.Permissions.AddQueryOption('$filter', $q) | Select;
-Contract-Assert (!!permissions)
+Contract-Assert (!!$permissions)
 
 # searches for the role name, unless you are UberAdmin you will only retrieve the role inside your tenant
 $q = "Name eq '{0}'" -f $RoleName;
 $roles = $svc.Core.Roles.AddQueryOption('$filter', $q).AddQueryOption('$expand', "Permissions") | Select;
-Contract-Assert (!!roles)
+Contract-Assert (!!$roles)
 
 foreach($role in $roles)
 {
