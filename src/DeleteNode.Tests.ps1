90ada290d567e150ded1f65aa298a9386efbe393
rufer7 <marc.rufer@d-fens.net>
2016-08-10 15:41:41 +0200

    minor

--- src/DeleteNode.Tests.ps1
+++ src/DeleteNode.Tests.ps1
@@ -259,12 +259,22 @@ Describe -Tags "DeleteNode.Tests" "DeleteNode.Tests" {
 			$query = "Id eq {0}" -f $entityBagId;
 			$entityBag = $svc.Core.EntityBags.AddQueryOption('$filter', $query) | select;
 			$entityBag | Should Be $null;
-			
-			
 		}
-		
-
-		
-		
 	}
 }
+
+#
+# Copyright 2016 d-fens GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
